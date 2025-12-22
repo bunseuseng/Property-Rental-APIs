b6b7cd9edb00f3eb7ede9d3f1565e85485e02677
bunseuseng <seng.bunseu@institute.pse.ngo>
2025-12-22 14:53:40 +0700

    first commit.

---
+++ src/main/java/com/group5/rental_room/RentalRoomApplication.java
@@ -0,0 +1,13 @@
+package com.group5.rental_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group5/rental_room/RentalRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.rental_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
