b22b043629ba7c0bf5e3667b840cee214ef684e3
bunseuseng <seng.bunseu@institute.pse.ngo>
2026-01-05 15:27:29 +0700

    update create method update in serviceImpl

---
+++ src/main/java/com/group5/rental_room/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.group5.rental_room.exception;
+
+import java.io.Serial;
+
+public class ResourceNotFoundException extends RuntimeException {
+	 @Serial
+     private static final long serialVersionUID = 1L;
+	 public ResourceNotFoundException(String message) {
+	        super(message);
+	    }
+}
--- src/main/java/com/group5/rental_room/service/PropertyServiceImpl.java
+++ src/main/java/com/group5/rental_room/service/PropertyServiceImpl.java
@@ -1,12 +1,14 @@
 package com.group5.rental_room.service;
 
 import com.group5.rental_room.dto.request.PropertyRequest;
+import com.group5.rental_room.dto.response.PropertyImageDTO;
 import com.group5.rental_room.dto.response.PropertyResponseDTO;
 import com.group5.rental_room.entity.PropertiesEntity;
 import com.group5.rental_room.entity.PropertyImageEntity;
 import com.group5.rental_room.entity.UserEntity;
 import com.group5.rental_room.repositpory.PropertyRepository;
 import com.group5.rental_room.repositpory.UserRepository;
+import com.group5.rental_room.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -53,6 +55,29 @@ public class PropertyServiceImpl implements PropertyService{
 	@Override
 	public PropertyResponseDTO updateProperty(Long id, PropertyRequest request, String email) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		PropertiesEntity property = propertyRepository.findById(id)
+				.orElseThrow(()-> new ResourceNotFoundException("Property not found with this id" + id));
+		if(!property.getAgent().getEmail().equals(email)) {
+      	  throw new RuntimeException("You are not allowed to update this property");
+		}
+		property.setTitle(request.getTitle());
+		property.setDescription(request.getDescription());
+		property.setPrice(request.getPrice());
+		property.setElectricityPrice(request.getElectricityPrice());
+		property.setWaterPrice(request.getWaterPrice());
+		property.setLocation(request.getLocation());
+		 property.getImages().clear();// orphanRemoval = true → old images deleted
+         request.getImageUrls().forEach(url ->
+             property.getImages().add(
+                 PropertyImageEntity.builder()
+                     .imageUrl(url)
+                     .property(property)
+                     .build()
+             )
+       		  );
+         
+         PropertiesEntity updated = propertyRepository.save(property);
+         return null;
 	}
 }
