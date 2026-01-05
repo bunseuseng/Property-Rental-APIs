96a028ed5692d0e987a2e983dbb5bb330dd6dcdf
bunseuseng <seng.bunseu@institute.pse.ngo>
2026-01-05 14:32:19 +0700

    add propertyResDTO, Agentdto and propertyImageres.

---
+++ src/main/java/com/group5/rental_room/dto/response/PropertyResponseDTO.java
@@ -0,0 +1,21 @@
+package com.group5.rental_room.dto.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PropertyResponseDTO {
+    private Long id;
+    private String title;
+    private String description;
+    private Double price;
+    private Double electricityPrice;
+    private Double  waterPrice; 
+    private String location;
+    private AgentDTO agent; 
+    private List<PropertyImageDTO> images;
+}
--- src/main/java/com/group5/rental_room/service/PropertyService.java
+++ src/main/java/com/group5/rental_room/service/PropertyService.java
@@ -1,9 +1,12 @@
 package com.group5.rental_room.service;
 
 import com.group5.rental_room.dto.request.PropertyRequest;
+import com.group5.rental_room.dto.response.PropertyResponseDTO;
+
 import com.group5.rental_room.entity.PropertiesEntity;
 
 public interface PropertyService {
     PropertiesEntity createProperty (PropertyRequest propertyRequest, String email);
+    PropertyResponseDTO updateProperty(Long id, PropertyRequest request, String email);
 
 }
