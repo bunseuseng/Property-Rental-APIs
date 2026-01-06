57936c30e97e9210cd7e1e9b3442ab3abea4b2f0
bunseuseng <seng.bunseu@institute.pse.ngo>
2026-01-06 08:41:40 +0700

    I have done on building review create end-points. Please, pull from develop after I merge create review with develop.

---
+++ src/main/java/com/group5/rental_room/service/CommentRepository.java
@@ -0,0 +1,7 @@
+//package com.group5.rental_room.service;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//public class CommentRepository {
+//    List<Comment> findByReviewIdAndParentCommentIsNull(Long reviewId);
+//
+//}
