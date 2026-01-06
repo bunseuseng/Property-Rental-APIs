c19e3911c02f2aff22de25680ec7582b5c7a51ad
bunseuseng <seng.bunseu@institute.pse.ngo>
2026-01-06 07:57:30 +0700

    I have done with created review entity and review repository please everyone pull before we to continues on building each review end-points.

---
+++ src/main/java/com/group5/rental_room/entity/ReviewEntity.java
@@ -0,0 +1,53 @@
+package com.group5.rental_room.entity;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "reviews")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReviewEntity {
+	  @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Long id;
+
+	    private int rating;
+
+	    @Column(name = "comment", columnDefinition = "TEXT")
+	    private String comment; // initial comment by user
+
+	    @ManyToOne(fetch = FetchType.LAZY)
+	    @JoinColumn(name = "user_id", nullable = false)
+	    private UserEntity user; // reviewer
+
+	    @ManyToOne(fetch = FetchType.LAZY)
+	    @JoinColumn(name = "property_id", nullable = false)
+	    private PropertiesEntity property;
+
+	    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL, orphanRemoval = true)
+	    @JsonIgnoreProperties("review")
+//	    private List<Comment> comments = new ArrayList<>();
+
+	    private LocalDateTime createdAt;
+	    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/group5/rental_room/repositpory/ReviewRepository.java
@@ -0,0 +1,12 @@
+package com.group5.rental_room.repositpory;
+
+import com.group5.rental_room.entity.ReviewEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReviewRepository {
+	  boolean existsByUserIdAndPropertyId(Long userId, Long propertyId);
+
+	  Page<ReviewEntity> findByPropertyId(Long propertyId, Pageable pageable);
+}
